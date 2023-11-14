bf51a78dd433211eacaa65eda5841c6f23ce2879
cssedi <u21450511@tuks.co.za>
2023-11-14 16:10:11 +0200

    added vintage kitman logo and added screen breakpoints

--- vintage-kitman-frontend/tailwind.config.js
+++ vintage-kitman-frontend/tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    }
   },
   plugins: [],
 }
