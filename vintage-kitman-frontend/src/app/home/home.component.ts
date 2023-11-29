cb2e95c6d18b320cd88923c5f68ca64a266e1b6d
cssedi <u21450511@tuks.co.za>
2023-11-29 10:58:24 +0200

    Add carousel and background image to home component

---
+++ vintage-kitman-frontend/src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import {ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: ['./home.component.scss'],
+
+
+})
+export class HomeComponent {
+
+}
