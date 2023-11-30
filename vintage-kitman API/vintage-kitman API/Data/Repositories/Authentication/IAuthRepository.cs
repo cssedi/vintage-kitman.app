0c34259f1bf37b59c95224f7c0382d34f950de26
cssedi <u21450511@tuks.co.za>
2023-11-30 17:19:30 +0200

    Registration and Customer Logging in foundations complete

---
+++ vintage-kitman API/vintage-kitman API/Data/Repositories/Authentication/IAuthRepository.cs
@@ -0,0 +1,10 @@
+﻿using vintage_kitman_API.ViewModels;
+
+namespace vintage_kitman_API.Data.Repositories.Authentication
+{
+    public interface IAuthRepository
+    {
+        public Task<UserManagerReponse> RegisterUserAsync(RegisterVM registerVM);
+        public Task<UserManagerReponse> LoginUserAsync(LoginVM loginVM);
+    }
+}
