6690bf5611bf0570938dd7050803b552e4875ed7
cssedi <u21450511@tuks.co.za>
2023-11-30 17:20:05 +0200

    Add new files and models for user authentication and registration

---
+++ vintage-kitman API/vintage-kitman API/ViewModels/UserManagerReponse.cs
@@ -0,0 +1,20 @@
+﻿namespace vintage_kitman_API.ViewModels
+{
+    public class UserManagerReponse
+    {
+        public string token { get; set; }
+        public string? Message { get; internal set; }
+        public bool isSuccess { get; internal set; }
+        public DateTime Date { get; internal set; }
+
+        public string? username { get; set; }
+
+        public string? name { get; set; }
+        public string? password { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public string? email { get; set; }
+
+        public string surname { get; set; }
+    }
+}
