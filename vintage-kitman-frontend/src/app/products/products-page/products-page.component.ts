19e9e4e7a69eb505ea45a034ef52272ed2b6cd27
cssedi <u21450511@tuks.co.za>
2023-12-04 23:15:29 +0200

    Add product display section to products page component

--- vintage-kitman-frontend/src/app/products/products-page/products-page.component.ts
+++ vintage-kitman-frontend/src/app/products/products-page/products-page.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { kitVM } from 'src/app/models/categories/kit-vm';
+import { CategoriesService } from 'src/app/services/Categories/categories.service';
 
 @Component({
   selector: 'app-products-page',
   templateUrl: './products-page.component.html',
   styleUrls: ['./products-page.component.scss']
 })
-export class ProductsPageComponent {
+export class ProductsPageComponent implements OnInit {
+  
+  leagueName!:string;
+  kitArray:kitVM[]=[]
+
+  constructor(private route:ActivatedRoute,private categoriesService:CategoriesService) { }
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.leagueName = params.get('name')?.replace('%20', ' ')!;
+      // Fetch products based on the leagueId using your ProductService
+      this.categoriesService.getKitsByLeagueName(this.leagueName).subscribe({
+        // Handle the retrieved products
+        next:(reponse)=>
+        {
+          this.kitArray=reponse as kitVM[]
+          console.log(this.leagueName)
+          console.log(reponse)
+        }
+      }
+
+      )
+    });  }
 
 }
