6d8e88be4097d7509e2b2b4de414a611989d009e
cssedi <102983169+cssedi@users.noreply.github.com>
2023-12-08 21:19:05 +0200

    Add product service and teams component

---
+++ vintage-kitman-frontend/src/app/services/product/product.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { kitVM } from 'src/app/models/categories/kit-vm';
+import { TeamsVM } from 'src/app/models/categories/teams-vm';
+import { environment } from 'src/environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  constructor(private http: HttpClient) { }
+  baseAPIURL = environment.baseAPIUrl+ "Products/"
+
+  getTeamsByLeagueName(name:string):Observable<TeamsVM[]>{
+    return this.http.get<TeamsVM[]>(this.baseAPIURL+"GetTeamsByLeague/"+name)
+  }
+
+  getKitsByTeam(id:number):Observable<kitVM[]>{
+    return this.http.get<kitVM[]>(this.baseAPIURL+"GetKitsByTeam/"+id)
+  }
+}
--- vintage-kitman-frontend/src/app/teams/teams.component.ts
+++ vintage-kitman-frontend/src/app/teams/teams.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../services/product/product.service';
+import { TeamsVM } from '../models/categories/teams-vm';
 
 @Component({
   selector: 'app-teams',
   templateUrl: './teams.component.html',
   styleUrls: ['./teams.component.scss']
 })
-export class TeamsComponent {
+export class TeamsComponent implements OnInit {
+  leagueName!:string;
+  teamArray:TeamsVM[]=[]
 
+  constructor(private route:ActivatedRoute,private productsService:ProductService) { }
+  
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.leagueName = params.get('name')?.replace('%20', ' ')!;
+      // Fetch products based on the leagueId using your ProductService
+      this.productsService.getTeamsByLeagueName(this.leagueName).subscribe({
+        // Handle the retrieved products
+        next:(reponse)=>
+        {
+          this.teamArray=reponse as TeamsVM[]
+          console.log(this.leagueName)
+          console.log(reponse)
+        }
+      }
+
+      )
+    });  
+  }
 }
