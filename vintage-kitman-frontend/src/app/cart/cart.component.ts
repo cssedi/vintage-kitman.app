f239a5338a01f1f5d409a778bc590bf3899ed7e7
cssedi <102983169+cssedi@users.noreply.github.com>
2023-12-18 15:09:22 +0200

    Update cart component with quantity changes and total cost calculation

--- vintage-kitman-frontend/src/app/cart/cart.component.ts
+++ vintage-kitman-frontend/src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartItem } from '../models/orders/CartItem-vm';
+import { CartService } from '../services/cart/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -8,30 +9,75 @@ import { CartItem } from '../models/orders/CartItem-vm';
 })
 export class CartComponent implements OnInit {
   cartArray: CartItem[] = [];
+  totalCost: number = 0;
+
+  constructor(private cartService:CartService) {
+
+    
+  }
   ngOnInit(): void 
   {
     const cart: CartItem[] = JSON.parse(localStorage.getItem('cart') || '[]');
     this.cartArray = cart;
     console.log(this.cartArray)
+    this.cartArray.forEach(item => {
+      this.totalCost =+ this.totalCost + (item.Quantity * item.KitPrice);
+    });
   }
 
-  decreaseQuantity(cartItem: CartItem) {
-    if (cartItem.Quantity > 1) {
-      var someItem = this.cartArray.filter(item => item.KitName === cartItem.KitName)
-      this.cartArray.splice(this.cartArray.indexOf(someItem[0]), 1);
-      localStorage.setItem('cart', JSON.stringify(this.cartArray));
-      window.location.reload();
+  decreaseQuantity(cartItem: CartItem) 
+  {
+    //decrease quantity
+    cartItem.Quantity--;
+    //remove cart item if quantity is 0
+    if(cartItem.Quantity ==0)
+    {
+      this.cartArray.splice(this.cartArray.indexOf(cartItem),1)
     }
-    this.cartArray.push(cartItem);
+    //update cart
     localStorage.setItem('cart', JSON.stringify(this.cartArray));
+    this.cartService.updateCartItemsCount(this.cartArray.length);
+
+    //calculate total cost
+    this.totalCost = 0;
+    this.cartArray.forEach(item => {
+      this.totalCost = this.totalCost + (item.Quantity * item.KitPrice);
+    });
+    
+    
+    console.log(this.totalCost);
   }
 
   increaseQuantity(cartItem: CartItem) {
+    //increase quantity
     cartItem.Quantity++;
-    this.cartArray.pop();
-    this.cartArray.push(cartItem);
+    //update cart
     localStorage.setItem('cart', JSON.stringify(this.cartArray));
+    this.cartService.updateCartItemsCount(this.cartArray.length);
+    //calculate total cost
+    this.totalCost = 0;
+    this.cartArray.forEach(item => {
+      this.totalCost = this.totalCost + (item.Quantity * item.KitPrice);
+    });
+
+    console.log(this.totalCost);
 
   }
+
+  deleteItem(cartItem: CartItem) {
+    //remove cart item
+    this.cartArray.splice(this.cartArray.indexOf(cartItem),1)
+    //update cart
+    localStorage.setItem('cart', JSON.stringify(this.cartArray));
+    this.cartService.updateCartItemsCount(this.cartArray.length);
+    //calculate total cost
+    this.totalCost = 0;
+    this.cartArray.forEach(item => {
+      this.totalCost = this.totalCost + (item.Quantity * item.KitPrice);
+    });
+    console.log(this.totalCost);
+  }
+
+
 
 }
