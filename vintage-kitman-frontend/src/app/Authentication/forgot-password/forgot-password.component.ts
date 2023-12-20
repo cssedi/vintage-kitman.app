98366eed868846fcc8de6c988c2161e81b9db529
cssedi <102983169+cssedi@users.noreply.github.com>
2023-12-20 15:27:26 +0200

    User Interface for forgot password

--- vintage-kitman-frontend/src/app/Authentication/forgot-password/forgot-password.component.ts
+++ vintage-kitman-frontend/src/app/Authentication/forgot-password/forgot-password.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ForgotPasswordVM } from 'src/app/models/authentication/forgotpassword-vm';
+import { AuthService } from 'src/app/services/authentication/auth.service';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.scss']
 })
-export class ForgotPasswordComponent {
+export class ForgotPasswordComponent implements OnInit {
 
   ifIsLoading:boolean = false;
+  resetPasswordForm!: FormGroup;
+  forgotPasswordDetails:ForgotPasswordVM ={email: '',message: null}
+
+  constructor(private authService:AuthService,private fb:FormBuilder) { }
+  ngOnInit(): void 
+  {
+    this.resetPasswordForm = this.fb.group(
+      {
+        email: ['', Validators.required]
+      })  
+  }
+
+  onSubmit(){
+    
+    this.ifIsLoading = true;
+    this.forgotPasswordDetails.email = this.resetPasswordForm.value.email;
+    this.authService.forgotPassword(this.forgotPasswordDetails).subscribe(
+      {
+        next: (res:any)=>
+        {
+          console.log(res)
+        },
+        complete: ()=>
+        {
+          this.ifIsLoading = false
+        },
+        error: (err:any)=>
+        {
+          console.log(err)
+          this.ifIsLoading = false
+        }
+      })
+
+  }
 }
