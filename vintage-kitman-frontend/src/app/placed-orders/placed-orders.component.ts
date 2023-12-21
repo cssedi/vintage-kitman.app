50aca8798310883469c7c987b06ff1bcffe9ff2b
cssedi <102983169+cssedi@users.noreply.github.com>
2023-12-21 16:48:17 +0200

    Add placed-orders component

---
+++ vintage-kitman-frontend/src/app/placed-orders/placed-orders.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-placed-orders',
+  templateUrl: './placed-orders.component.html',
+  styleUrls: ['./placed-orders.component.scss']
+})
+export class PlacedOrdersComponent {
+
+}
