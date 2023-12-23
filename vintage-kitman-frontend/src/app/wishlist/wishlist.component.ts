86463e24be3e4f9ef6c74786e880e7cd40c13457
cssedi <102983169+cssedi@users.noreply.github.com>
2023-12-23 15:08:53 +0200

    Update wishlist component HTML and SCSS files, and add OnInit implementation

--- vintage-kitman-frontend/src/app/wishlist/wishlist.component.ts
+++ vintage-kitman-frontend/src/app/wishlist/wishlist.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OrderService } from '../services/order/order.service';
+import { kitVM } from '../models/categories/kit-vm';
 
 @Component({
   selector: 'app-wishlist',
   templateUrl: './wishlist.component.html',
   styleUrls: ['./wishlist.component.scss']
 })
-export class WishlistComponent {
+export class WishlistComponent implements OnInit {
+  kitArray: kitVM[] = [];
+  constructor(private ordersService: OrderService) {}
+  ngOnInit(): void {
+    this.ordersService.getWishlist().subscribe({
+      next: (response) => {
+        this.kitArray = response as kitVM[];
+        console.log(response);
+      },
+      complete: () => {},
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
 
 }
