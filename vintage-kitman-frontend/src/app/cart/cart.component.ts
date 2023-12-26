43f21c74576585ca555da4b568978d4a644c3ad7
cssedi <102983169+cssedi@users.noreply.github.com>
2023-12-26 06:33:09 +0200

    dynamic retreival of cart total

--- vintage-kitman-frontend/src/app/cart/cart.component.ts
+++ vintage-kitman-frontend/src/app/cart/cart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartItem } from '../models/orders/CartItem-vm';
 import { CartService } from '../services/cart/cart.service';
+import { OrderService } from '../services/order/order.service';
 
 @Component({
   selector: 'app-cart',
@@ -10,8 +11,9 @@ import { CartService } from '../services/cart/cart.service';
 export class CartComponent implements OnInit {
   cartArray: CartItem[] = [];
   totalCost: number = 0;
+  ifIsLoading:boolean = false
 
-  constructor(private cartService:CartService) {
+  constructor(private cartService:CartService, private orderService:OrderService) {
 
     
   }
@@ -19,14 +21,24 @@ export class CartComponent implements OnInit {
   {
     const cart: CartItem[] = JSON.parse(localStorage.getItem('cart') || '[]');
     this.cartArray = cart;
-    console.log(this.cartArray)
-    this.cartArray.forEach(item => {
-      this.totalCost =+ this.totalCost + (item.Quantity * item.KitPrice);
-    });
+    this.ifIsLoading = true
+
+    //get cart total
+    this.orderService.getCartTotal(this.cartArray)
+    .subscribe
+    ({
+      next:(response)=>
+      { 
+        this.totalCost = response.total
+      },
+      complete: ()=>{this.ifIsLoading=false},
+      error:(err)=>{this.ifIsLoading = false}
+    })
   }
 
   decreaseQuantity(cartItem: CartItem) 
   {
+    this.ifIsLoading=true
     //decrease quantity
     cartItem.Quantity--;
     //remove cart item if quantity is 0
@@ -39,43 +51,61 @@ export class CartComponent implements OnInit {
     this.cartService.updateCartItemsCount(this.cartArray.length);
 
     //calculate total cost
-    this.totalCost = 0;
-    this.cartArray.forEach(item => {
-      this.totalCost = this.totalCost + (item.Quantity * item.KitPrice);
-    });
-    
-    
-    console.log(this.totalCost);
+    //get cart total
+    this.orderService.getCartTotal(this.cartArray)
+    .subscribe
+    ({
+      next:(response)=>
+      {
+        console.log(response.total)
+        this.totalCost = response.total
+      },
+      complete: ()=>{this.ifIsLoading=false},
+      error:(err)=>{this.ifIsLoading = false}
+    })
+        
   }
 
   increaseQuantity(cartItem: CartItem) {
+    this.ifIsLoading=true
     //increase quantity
     cartItem.Quantity++;
     //update cart
     localStorage.setItem('cart', JSON.stringify(this.cartArray));
     this.cartService.updateCartItemsCount(this.cartArray.length);
-    //calculate total cost
-    this.totalCost = 0;
-    this.cartArray.forEach(item => {
-      this.totalCost = this.totalCost + (item.Quantity * item.KitPrice);
-    });
+    //update cart total
+    this.orderService.getCartTotal(this.cartArray)
+    .subscribe
+    ({
+      next:(response)=>
+      {
+        this.totalCost = response.total
+      },
+      complete: ()=>{this.ifIsLoading=false},
+      error:(err)=>{this.ifIsLoading = false}
+    })
 
-    console.log(this.totalCost);
 
   }
 
   deleteItem(cartItem: CartItem) {
+    this.ifIsLoading = true
     //remove cart item
     this.cartArray.splice(this.cartArray.indexOf(cartItem),1)
     //update cart
     localStorage.setItem('cart', JSON.stringify(this.cartArray));
     this.cartService.updateCartItemsCount(this.cartArray.length);
-    //calculate total cost
-    this.totalCost = 0;
-    this.cartArray.forEach(item => {
-      this.totalCost = this.totalCost + (item.Quantity * item.KitPrice);
-    });
-    console.log(this.totalCost);
+    //update cart total
+    this.orderService.getCartTotal(this.cartArray)
+    .subscribe
+    ({
+      next:(response)=>
+      {
+        this.totalCost = response.total
+      },
+      complete: ()=>{this.ifIsLoading=false},
+      error:(err)=>{this.ifIsLoading = false}
+    })
   }
 
 
